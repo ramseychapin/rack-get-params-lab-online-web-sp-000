fdaa7384853333143289b0aae8b4fcc53ee76a8e
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-01 23:53:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else
-        resp.write "We don't have that item"
+        resp.write "We don't have that item."
     else
       resp.write "Path Not Found"
     end
