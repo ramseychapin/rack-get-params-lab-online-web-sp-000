43c48b8852fb0dd322f94b1aecb5e502a6f48945
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-01 23:48:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
       end
     elsif req.path.match(/add/)
 
