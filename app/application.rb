95a42dd0ef90c2ef13679837671883afed027bcf
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-01 23:53:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,7 @@ class Application
       item = req.params["item"]
       if @@items.include?(item)
         @@cart << item
+        resp.write "added #{item}"
     else
       resp.write "Path Not Found"
     end
