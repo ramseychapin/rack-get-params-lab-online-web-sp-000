caaf2005a05e16dc9ab6c4f8afe752b73357f12a
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-01 23:53:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,7 @@ class Application
     elsif req.path.match(/add/)
       item = req.params["item"]
       if @@items.include?(item)
+        @@cart << item
     else
       resp.write "Path Not Found"
     end
